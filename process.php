e98fb31f8ac14ccb490b8482030ac7f67aeecf8b
Emmagist <emma1994204@gmail.com>
2022-02-18 22:53:04 +0100

    php mailer added

--- process.php
+++ process.php
@@ -13,6 +13,78 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
+    require_once "vendor/autoload.php";
+
+    // Create the Transport
+    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+    ->setUsername('emma1994204@gmail.com')
+    ->setPassword('Emmagist1994204');
+    ;
+
+    // Create the Mailer using your created Transport
+    $mailer = new Swift_Mailer($transport);
+
+    function sendAdminMessage($email){
+        global $mailer;
+        $body = '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Subscribe Yetti</title>
+        </head>
+        <body>
+            <div class="email_wrap">
+                <h2>Hi Admin,</h2>
+                <p>You have a new subscriber</p>
+                <h3>Email: <strong>' . $email . '</strong></h4>
+            </div> 
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        </body>
+        </html>';
+        // Create a message
+        $message = (new Swift_Message('You have a new message'))
+        ->setFrom('emma1994204@gmail.com')
+        ->setTo('emma1994204@gmail.com')
+        ->setBody($body, 'text/html');
+        // Send the message
+        $result = $mailer->send($message);
+
+        return $result;
+    }
+
+    function appreciationMessage($email){
+        global $mailer;
+        $body = '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Subscribe Yetti</title>
+        </head>
+        <body>
+            <div class="email_wrap">
+                <h2>Hi,</h2>
+                <p>Thanks for subscribing us @ <img src="assets/svgs/logo.svg" alt="Yetti Logo" class="logo" />. <br> Your information is secured with us.</p>
+            </div> 
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        </body>
+        </html>';
+        // Create a message
+        $message = (new Swift_Message('Thanks for reaching to us.'))
+        ->setFrom('Willzfrankenterprise4@gmail.com')
+        ->setTo($email)
+        ->setBody($body, 'text/html');
+        // Send the message
+        $result = $mailer->send($message);
+
+        return $result;
+    }
     
     if (isset($_POST['subscribe']) || isset($_POST['subscribe_button'])) {
         $error = '';
@@ -29,6 +101,9 @@
 
         if (empty($error)) {
             $sql = "INSERT INTO news_letter (email) VALUES ('$email')"; //var_dump($sql);exit;
+
+            sendAdminMessage($email);
+            appreciationMessage($email);
 
             if ($conn->query($sql) === TRUE) {
                 header('Location: index.php');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd86080530fc0bbb5daaf41cad844b7488f1823aa',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd86080530fc0bbb5daaf41cad844b7488f1823aa',
+    ),
+    'doctrine/lexer' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9c50f840f257bbb941e6f4a0e94ccf5db5c3f76c',
+    ),
+    'egulias/email-validator' => 
+    array (
+      'pretty_version' => '2.1.25',
+      'version' => '2.1.25.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0dbf5d78455d4d6a41d186da50adc1122ec066f4',
+    ),
+    'swiftmailer/swiftmailer' => 
+    array (
+      'pretty_version' => 'v6.0.0',
+      'version' => '6.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '74e20ce4dad5011fb2c2cedefb76b2237f123c0e',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.24.0',
+      'version' => '1.24.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '749045c69efb97c70d25d7463abba812e91f3a44',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.24.0',
+      'version' => '1.24.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8590a5f561694770bdcd3f9b5c69dde6945028e8',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.24.0',
+      'version' => '1.24.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a142215a36a3888e30d0a9eeea9766764e96976',
+    ),
+  ),
+);
